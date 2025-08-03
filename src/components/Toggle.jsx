7559f5c6a907216b32c75612bf25dee06e650eb4
Bibek2307 <bibek.mukherjee2307@gmail.com>
2025-08-03 18:49:36 +0530

    commit

---
+++ src/components/Toggle.jsx
@@ -0,0 +1,36 @@
+import React from 'react'
+import { useState } from 'react'
+import "../style.css"
+
+const Toggle = () => {
+
+    const[textColor, setTextColor]= useState("#1b1b1b")
+    const[backgroundColor,setBackgroundColor]=useState("white")
+
+
+    const handleclick=()=>{
+        setBackgroundColor(backgroundColor==="white"?"#1b1b1b":"white")
+        setTextColor(textColor=="#1b1b1b"?"#ffa31a":"#1b1b1b")
+        
+    }
+
+  return (
+    <section style={{backgroundColor, color: textColor}}>
+        <button className='btn' onClick={handleclick} style={{backgroundColor , color: textColor, border:`2px solid ${textColor}`}}>
+            {backgroundColor=="#1b1b1b"?"Dark Theme":"White Theme"}
+        </button>
+
+        <section className="content">
+            <h1>
+                Use The Button To Change Theme
+            </h1>
+        </section>
+      
+    </section>
+  )
+}
+
+export default Toggle
+
+
+
